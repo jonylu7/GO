d808b5ab45fff69e9969b9c70cd460de0c215fdc
jonylu7 <jonylu7@gmail.com>
2023-12-18 16:57:42 +0800

    basics

--- Game/Board.py
+++ Game/Board.py
@@ -1,10 +1,1 @@
-from collections import namedtuple
-
-class Point(namedtuple('Point','row col')):
-    def neighbors(self):
-        return [
-            Point(self.row-1,self.col),
-            Point(self.row, self.col-1),
-            Point(self.row+1, self.col),
-            Point(self.row, self.col+1),
-        ]
+class Board:
