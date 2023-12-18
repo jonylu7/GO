55bdc66611cb503a920798bb23357b563f998c1a
jonylu7 <jonylu7@gmail.com>
2023-12-18 17:09:36 +0800

    finished GoString

--- Game/Board.py
+++ Game/Board.py
@@ -1,1 +1,12 @@
-class Board:
+class Board():
+    def __init__(self,num_rows,num_cols):
+        self.num_rows=num_rows
+        self.num_cols=num_cols
+        self._grid={}
+
+    def place_stone(self,player,point):
+        assert self.is_on_grid(point)
+        assert self._grid.get(point) is None
+
+
+    def is_on_grid(self,point):
--- Game/GroupTracking.py
+++ Game/GroupTracking.py
@@ -1,25 +1,23 @@
+class GoString():
+    def __init__(self, color, stones, liberties):
+        self.color = color
+        self.stones = set(stones)
+        self.liberties = set(liberties)
 
- class GoString():
-     def __init__(self,color,stones,liberties):
-         self.color=color
-         self.stones=set(stones)
-         self.liberties=set(liberties)
-     def remove_liberty(self,point):
-         self.liberties.remove(point)
+    def remove_liberty(self, point):
+        self.liberties.remove(point)
 
-     def add_liberty(self,point):
-         self.liberties.add(point)
+    def add_liberty(self, point):
+        self.liberties.add(point)
 
-     def merged_with(self,go_string):
-         assert go_string.color==self.color
-         combined_stones=self.stones | go_string.stones
-         return GoString(self.color,combined_stones,(self.liberties | go_string.liberties)-combined_stones)
+    def merged_with(self, go_string):
+        assert go_string.color == self.color
+        combined_stones = self.stones | go_string.stones
+        return GoString(self.color, combined_stones, (self.liberties | go_string.liberties) - combined_stones)
 
     @property
     def num_liberties(self):
-     return len(self.liberties)
+        return len(self.liberties)
 
-    def __eq__(self,other):
-        return isinstance(other,GoString) and \
-
-
+    def __eq__(self, other):
+        return isinstance(other, GoString) and (self.stones == other.stones) and (self.liberties == other.liberties)
